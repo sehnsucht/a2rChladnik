bd400e756618a1d6b1cbf7ec5dc4b3435745e97a
sehnsucht <2brandt63@gmail.com>
2023-08-28 13:35:54 +0200

    Update chladni.js

--- chladni.js
+++ chladni.js
@@ -1,35 +1,27 @@
-// Import the AudioStream class
-import AudioStream from "https://cdnjs.cloudflare.com/ajax/libs/three.js/137/examples/js/loaders/AudioStream.js";
+import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/137/three.min.js";
 
-// Create an AudioStream object
-const audioStream = new AudioStream();
+const audioStream = new THREE.AudioStream();
 audioStream.open(new URL("http://a2r.twenty4seven.cc:8000/puredata.ogg", document.baseURI));
 
-// Set the parameters of the Chladni figure
 const m = [0, 1, 2, 3, 4, 5, 6, 7];
 
-// Create a scene and renderer
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a sphere geometry and material
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 const geometry = new THREE.SphereGeometry(100, 100, 100);
 const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
 
-// Create a mesh and add it to the scene
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Update the Chladni figure at every new audio data rate
-audioStream.ondataavailable = function() {
-  // Calculate the radius of each point on the figure
-  const r = f.map(f => Math.pow(Math.sin(m[0] * f), m[1]) + Math.pow(Math.cos(m[2] * f), m[3]) + Math.pow(Math.sin(m[4] * f), m[5]) + Math.pow(Math.cos(m[6] * f), m[7]));
+audioStream.ondataavailable = () => {
+  const f = audioStream.getBands(8);
 
-  // Update the mesh's vertices
-  mesh.geometry.vertices = r.map(r => new THREE.Vector3(r * Math.sin(f), r * Math.cos(f), 0));
+  mesh.geometry.vertices = f.map(f => new THREE.Vector3(f * Math.sin(f), f * Math.cos(f), 0));
 
-  // Render the scene
   renderer.render(scene, camera);
 };
